c8b13853c08c88a94c1cb9ed25a43001aa545085
kookiejeon <mynameisjungkook197@gmail.com>
2026-03-10 13:20:04 +0800

    lab-act1

--- navarro-client/src/app.jsx
+++ navarro-client/src/app.jsx
@@ -8,36 +8,20 @@ export function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
+     <div className="App">
+        <header className="App-header">
+          <h1>Welcome to My React App!</h1>
+          <p>
+            Name: Kristine Navarro<br />
+            Email: kristinevineonavarro@gmail.com<br />
+            Other info: <a href="https://github.com/Metalxr/maquilan-webprog">GitHub Repository</a>
+          </p>
+
+
+        </header>
+       
       </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+ 
     </>
   )
 }
